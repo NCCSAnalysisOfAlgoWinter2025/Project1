c83e9fbde4beafaeefda8ac0f660b84be52ea060
Joe Klinck <jklinck76@gmail.com>
2025-01-19 05:29:47 -0800

    completed InsertionSort class along with javadoc comments

--- src/main/java/SortingAlgorithms/InsertionSort.java
+++ src/main/java/SortingAlgorithms/InsertionSort.java
@@ -1,4 +1,33 @@
 package SortingAlgorithms;
 
+/**
+ * I implement this algorithm from memory.
+ */
+
 public class InsertionSort {
+    /**
+     * Defualt constructor.
+     * @param inputArray
+     */
+    public InsertionSort(int[] inputArray){
+    }
+
+    /**
+     * InsertionSort algorithm
+     * @param inputArray
+     */
+    public static void insertionSort(int[] inputArray){
+        int len = inputArray.length;
+        for(int i = 1;i < len;i++){
+            int temp = inputArray[i];
+            for(int j = i - 1;j >= 0;j--){
+                if(inputArray[j] > inputArray[j + 1]){
+                    inputArray[j + 1] = inputArray[j];
+                    inputArray[j] = temp;
+                    temp = inputArray[j];
+                }
+            }
+        }
+
+    }
 }
