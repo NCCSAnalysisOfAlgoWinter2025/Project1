0d30c8f578a9c56818a189804b3eb403a71853aa
Joe Klinck <jklinck76@gmail.com>
2025-01-21 00:57:29 -0800

    fixed javadoc check style issues

--- src/main/java/SortingAlgorithms/MergeSort.java
+++ src/main/java/SortingAlgorithms/MergeSort.java
@@ -9,78 +9,81 @@ package SortingAlgorithms;
 
 public class MergeSort {
 
-    /**
-     * Default constructor.
-     * @param inputArray
-     */
-    public MergeSort(int[] inputArray){
+  /**
+   * Default constructor.
+   *
+   * @param inputArray the input array of integers to be sorted
+   */
+  public MergeSort(int[] inputArray) {
+  }
+
+  /**
+   * Sorts an int array with the MergeSort method. Uses the Merge
+   * helper method below for merging arrays.
+   *
+   * @param inputArray the input array of integers to be sorted
+   */
+  public static void mergeSort(int[] inputArray) {
+    int inputLength = inputArray.length;
+
+    if (inputLength < 2) {
+      return;
     }
 
-    /**
-     * Sorts an int array with the MergeSort method. Uses the Merge
-     * helper method below for merging arrays.
-     * @param inputArray
+    int midIndex = inputLength / 2;
+    int[] leftHalf = new int[midIndex];
+    int[] rightHalf = new int[inputLength - midIndex];
+
+    /*
+    This is code that the code style checker wrote over what I had originally
+    written, it does the same thing that the two for loops I wrote, I decided to
+    leave it as is
      */
-    private static void mergeSort(int[] inputArray) {
-        int inputLength = inputArray.length;
+    System.arraycopy(inputArray, 0, leftHalf, 0, midIndex);
+    System.arraycopy(inputArray, midIndex, rightHalf, 0, inputLength - midIndex);
 
-        if (inputLength < 2) {
-            return;
-        }
+    mergeSort(leftHalf);
+    mergeSort(rightHalf);
 
-        int midIndex = inputLength / 2;
-        int[] leftHalf = new int[midIndex];
-        int[] rightHalf = new int[inputLength - midIndex];
+    merge(inputArray, leftHalf, rightHalf);
+  }
 
-        for (int i = 0; i < midIndex; i++) {
-            leftHalf[i] = inputArray[i];
-        }
-        for (int i = midIndex; i < inputLength; i++) {
-            rightHalf[i - midIndex] = inputArray[i];
-        }
+  /**
+   * Helper method for MergeSort method that merges the arrays created
+   * back together.
+   *
+   * @param inputArray the input array of arrays to be merged
+   * @param leftHalf the left half of the input array to merged
+   * @param rightHalf the right half of the input array to be merged
+   */
+  private static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
+    int leftSize = leftHalf.length;
+    int rightSize = rightHalf.length;
 
-        mergeSort(leftHalf);
-        mergeSort(rightHalf);
+    int leftPos = 0, rightPos = 0, arrPos = 0;
 
-        merge(inputArray, leftHalf, rightHalf);
+    while (leftPos < leftSize && rightPos < rightSize) {
+      if (leftHalf[leftPos] <= rightHalf[rightPos]) {
+        inputArray[arrPos] = leftHalf[leftPos];
+        leftPos++;
+      } else {
+        inputArray[arrPos] = rightHalf[rightPos];
+        rightPos++;
+      }
+      arrPos++;
     }
 
-    /**
-     * Helper method for MergeSort method that merges the arrays created
-     * back together.
-     * @param inputArray
-     * @param leftHalf
-     * @param rightHalf
-     */
-    private static void merge (int[] inputArray, int[] leftHalf, int[] rightHalf) {
-        int leftSize = leftHalf.length;
-        int rightSize = rightHalf.length;
-
-        int leftPos = 0, rightPos = 0, arrPos = 0;
-
-        while (leftPos < leftSize && rightPos < rightSize) {
-            if (leftHalf[leftPos] <= rightHalf[rightPos]) {
-                inputArray[arrPos] = leftHalf[leftPos];
-                leftPos++;
-            }
-            else {
-                inputArray[arrPos] = rightHalf[rightPos];
-                rightPos++;
-            }
-            arrPos++;
-        }
-
-        while (leftPos < leftSize) {
-            inputArray[arrPos] = leftHalf[leftPos];
-            leftPos++;
-            arrPos++;
-        }
-
-        while (rightPos < rightSize) {
-            inputArray[arrPos] = rightHalf[rightPos];
-            rightPos++;
-            arrPos++;
-        }
-
+    while (leftPos < leftSize) {
+      inputArray[arrPos] = leftHalf[leftPos];
+      leftPos++;
+      arrPos++;
     }
+
+    while (rightPos < rightSize) {
+      inputArray[arrPos] = rightHalf[rightPos];
+      rightPos++;
+      arrPos++;
+    }
+
+  }
 }
