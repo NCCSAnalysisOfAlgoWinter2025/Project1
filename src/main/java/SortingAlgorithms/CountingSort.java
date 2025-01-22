af19da958b9e7b569137b36a0810060bdcd63a8f
Joe Klinck <jklinck76@gmail.com>
2025-01-21 17:10:39 -0800

    created CountingSort test directory

--- src/main/java/SortingAlgorithms/CountingSort.java
+++ src/main/java/SortingAlgorithms/CountingSort.java
@@ -1,4 +1,13 @@
 package SortingAlgorithms;
 
 public class CountingSort {
+
+  /**
+   * Sorts input array with the countingSort algorithm
+   *
+   * @param inputArray integer array to be sorted
+   */
+  public void countingSort(int[] inputArray){
+
+  }
 }
