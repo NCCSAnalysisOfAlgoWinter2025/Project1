c8e6eadc065b2b7e0a52210a0453ae4339bc8c56
Joe Klinck <jklinck76@gmail.com>
2025-01-24 03:09:34 -0800

    added best/worts/average runtimes

--- src/main/java/SortingAlgorithms/CountingSort.java
+++ src/main/java/SortingAlgorithms/CountingSort.java
@@ -3,6 +3,8 @@ package SortingAlgorithms;
 public class CountingSort {
 
   /**
+   * Big O (n + k), Big Omega (n + k) and Theta (n + k)
+   *
    * Sorts input array with the countingSort algorithm
    *
    * @param inputArray integer array to be sorted
