2646d755da6f47ef19591b172468cb7bec05db4e
Joe Klinck <jklinck76@gmail.com>
2025-01-23 21:12:55 -0800

    updated test to work with static insertionSort method

--- src/main/Test/SortingAlgorithms/InsertionSortTest.java
+++ src/main/Test/SortingAlgorithms/InsertionSortTest.java
@@ -6,9 +6,10 @@ class InsertionSortTest {
 
   @org.junit.jupiter.api.Test
   void insertionSort() {
-    InsertionSort sort = new InsertionSort();
+//    InsertionSort sort = new InsertionSort();
     int[] arr = {9,8,7,6};
-    sort.insertionSort(arr);
+//    sort.insertionSort(arr);
+    InsertionSort.insertionSort(arr);
     int[] expected = {6,7,8,9};
     assertArrayEquals(expected, arr);
   }
