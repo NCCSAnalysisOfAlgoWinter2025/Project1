98c3048b8fd9a4abcb0772531eebd8410ae119c6
Joe Klinck <jklinck76@gmail.com>
2025-01-25 17:33:47 -0800

    countingSort is almost finished, there is still a small bug

--- src/main/java/SortingAlgorithms/CountingSort.java
+++ src/main/java/SortingAlgorithms/CountingSort.java
@@ -1,5 +1,7 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class CountingSort {
 
   /**
@@ -7,9 +9,37 @@ public class CountingSort {
    *
    * Sorts input array with the countingSort algorithm
    *
+   * I wrote the code for this algorithm by following along with the
+   * explanation in the geeeksforgeeks.org website. I didn't copy the
+   * copy, I simply followed the graphical explanation of how the
+   * algorithm works and wrote my own code.
+   *
    * @param inputArray integer array to be sorted
    */
+
+
+
+  // this isn't finished yet, there is still a small bug
   public static void countingSort(int[] inputArray){
+    int max = Arrays.stream(inputArray).max().orElseThrow();;
+
+    int[] countArray = new int[max + 1];
+    int[] outputArray = new int[inputArray.length];
+
+    for(int i = 0;i < inputArray.length;i++){
+      countArray[inputArray[i]]++;
+    }
+
+    for(int j = 1;j < countArray.length;j++){
+      countArray[j] = countArray[j - 1] + countArray[j];
+    }
+
+    for(int k = inputArray.length - 1;k >= 0;k--){
+      outputArray[countArray[inputArray[k]] - 1] = inputArray[k];
+      countArray[inputArray[k]] = countArray[inputArray[k]]--;
+    }
+    inputArray = outputArray;
+    System.out.println(Arrays.toString(outputArray));
 
   }
 }
