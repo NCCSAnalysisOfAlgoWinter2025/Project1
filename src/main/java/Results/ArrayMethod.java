16c95561174bac930647ce0ebb55b24a1a9aca6c
Joe Klinck <jklinck76@gmail.com>
2025-02-04 18:00:45 -0800

    completed CONTRIBUTING.md

--- src/main/java/Results/ArrayMethod.java
+++ src/main/java/Results/ArrayMethod.java
@@ -2,6 +2,10 @@ package Results;
 
 import ArrayCreator.ArrayCreator;
 
+/**
+ * Functional interface so you can pass the specific array you want
+ * into the timer() method inside ChartCreator
+ */
 @FunctionalInterface
 public interface ArrayMethod {
   int[] execute(ArrayCreator arrayCreator);
--- src/main/java/Results/SortAlgorithm.java
+++ src/main/java/Results/SortAlgorithm.java
@@ -1,5 +1,9 @@
 package Results;
 
+/**
+ * Function interface for passing specific sorting algorithm into the constructor
+ * of the ChartCreator class
+ */
 @FunctionalInterface
 public interface SortAlgorithm {
   void sort(int[] array);
